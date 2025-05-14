13c91d528e49afe70372e186fb622b6c30ed1d4a
TheKub4 <77793837+TheKuba@users.noreply.github.com>
2025-05-14 10:35:54 +0200

    Dice Roll Minigame

--- src/core/DiceRoll.java
+++ src/core/DiceRoll.java
@@ -5,8 +5,6 @@ import model.GameResult;
 import ui.InputHandler;
 
 public class DiceRoll extends Game {
-    private static final int FIELD_COUNT = 7;
-
     public DiceRoll(String gameName, int minBet) {
         super(gameName, minBet);
     }
@@ -35,30 +33,13 @@ public class DiceRoll extends Game {
 
     @Override
     public GameResult startGame() {
-        System.out.println("Hazite kostkou, volte, zda chcete hodit ci ne");
-        System.out.println("Pokud vyskocite mimo hraci pole, hra konci s nulovou vyhrou");
         int position = 0;
-        int roll;
         int winnings = getInputBet();
         printTrack(position);
-        int choice = InputHandler.readChoices("Prejete si hodit?", "Ano", "Ne");
-        while (choice != 1) {
-            System.out.println("jumping...");
-            roll = RandomGen.getRandomInt(1,6);
-            position += roll;
-            System.out.println("Padlo: " + roll);
+        while (choice != 1){
             printTrack(position);
-            if (position <= FIELD_COUNT) {
-                choice = InputHandler.readChoices("Prejete si risknout dalsi pokus?", "Ano", "Ne");
             } else {
-                System.out.println("Skocil jste mimo pole jste prohral");
-                return new GameResult(this, 0, getInputBet(), false);
             }
         }
-        if (position <= 4) winnings *= 1.1;
-        else if (position <= 6) winnings *= 1.4;
-        else winnings *= 2;
-        System.out.println("Vyhráno: " + winnings);
-        return new GameResult(this, winnings, getInputBet(), true);
     }
 }
