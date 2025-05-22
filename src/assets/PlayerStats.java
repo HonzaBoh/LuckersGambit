a5d1f00e1ffb7f7b69ac4df8911ad3ba63337d61
jan.vondrasek <oceanmaaan27@gmail.com>
2025-05-22 10:33:37 +0200

    implementace

--- src/assets/PlayerStats.java
+++ src/assets/PlayerStats.java
@@ -17,8 +17,8 @@ public class PlayerStats {
         System.out.println("Total Games Played: " + history.size());
         System.out.println("Počet vyhraných her: " + countWins(history));
         System.out.println("Prohry: " + countLosses(history));
-        System.out.println("Zisk/ztráta" + totalWinnings(history));
-        System.out.println("Win rate" + getWinRate(history));
+        System.out.println("Zisk/ztráta " + totalWinnings(history));
+        System.out.println("Win rate " + getWinRate(history));
     }
 
     public static long countWins(List<GameResult> history) {
@@ -26,14 +26,25 @@ public class PlayerStats {
     }
 
     public static long countLosses(List<GameResult> history) {
-        return 0;
+        return history.size() - history.stream().filter(GameResult::isWin).count();
     }
 
     public static int totalWinnings(List<GameResult> history) {
-        return 0;
+
+        return history.stream()
+                .mapToInt(GameResult::getWinnings)
+                .sum();
     }
 
     public static String getWinRate(List<GameResult> history) {
-        return "X %";
+
+        long wins = history.stream().filter(GameResult::isWin).count();
+        long games = history.size();
+
+        if(games == 0){
+            return "hrac neodehral zadne hry";
+        }
+
+        return wins/games*100 + "%";
     }
 }
