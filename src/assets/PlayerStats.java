cc3d6d71884d72668ae3371cc9f1c99289d405f8
jan.feher <jan.feher@infis.cz>
2025-05-22 10:34:13 +0200

    implementace PlayerStats

--- src/assets/PlayerStats.java
+++ src/assets/PlayerStats.java
@@ -1,5 +1,6 @@
 package assets;
 
+import core.Game;
 import model.GameResult;
 import model.Player;
 
@@ -9,7 +10,11 @@ import java.util.stream.Collectors;
 public class PlayerStats {
 
     public static void printStats(Player player) {
-        List<GameResult> history = player.getHistory();
+        List<GameResult> history;
+        if ((history = player.getHistory()).isEmpty()) {
+            System.out.println("Zatim nebyla odehrana zadna hra");
+            return;
+        }
 
         System.out.println("=== Player Statistics ===");
         System.out.println("Hráč: " + player.getNickName());
@@ -17,8 +22,8 @@ public class PlayerStats {
         System.out.println("Total Games Played: " + history.size());
         System.out.println("Počet vyhraných her: " + countWins(history));
         System.out.println("Prohry: " + countLosses(history));
-        System.out.println("Zisk/ztráta" + totalWinnings(history));
-        System.out.println("Win rate" + getWinRate(history));
+        System.out.println("Zisk/ztráta: " + totalWinnings(history));
+        System.out.println("Win rate: " + getWinRate(history));
     }
 
     public static long countWins(List<GameResult> history) {
@@ -26,14 +31,28 @@ public class PlayerStats {
     }
 
     public static long countLosses(List<GameResult> history) {
-        return 0;
+        return history.stream()
+                .filter(result -> !result.isWin())
+                .count();
     }
 
     public static int totalWinnings(List<GameResult> history) {
-        return 0;
+        int winnings = history.stream()
+                .filter(GameResult::isWin)
+                .mapToInt(result -> result.getWinnings() - result.getBet())
+                .sum();
+        int losses = history.stream()
+                .filter(result -> !result.isWin())
+                .mapToInt(GameResult::getBet)
+                .sum();
+        return winnings - losses;
     }
 
     public static String getWinRate(List<GameResult> history) {
-        return "X %";
+        double average = history.stream()
+                .mapToDouble(result -> result.isWin() ? 1.0 : 0.0)
+                .average()
+                .orElse(Double.NaN);
+        return (average * 100) + " %";
     }
 }
